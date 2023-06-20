ac145baacd28fef2da858043f32290f1b82c9034
aksan <sukmailyas9@gmail.com>
2023-06-20 17:34:13 +0800

    first commit

---
+++ src/components/Movies.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Movies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        // const headers = new Headers();
+        // headers.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "Get",
+            // headers: headers,
+        }
+
+
+        fetch(`http://localhost/8080/movies`, requestOptions)
+            .then((respons) => respons.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }, []);
+
+    return (
+        <div>
+            <h2>Movies</h2>
+            <hr />
+            <table className="table tavle-stripped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((m) => (
+                        <tr key={m.id}>
+                            <td>
+                                <Link to={`/movies/${m.id}`}>
+                                    {m.title}
+                                </Link>
+                            </td>
+                            <td>{m.release_date}</td>
+                            <td>{m.mpaa_rating}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Movies;
