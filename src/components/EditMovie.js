598a897a2b2c298e7995079e764077fa00b85eb4
sukma ilyas <sukmailyas9@gmail.com>
2023-07-14 23:41:51 +0800

    validation II

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -4,6 +4,7 @@ import Input from "./form/Input";
 import Select from "./form/Select";
 import TextArea from "./form/TextArea"
 import Checkbox from "./form/Checkbox"
+import Swal from "sweetalert2";
 
 
 
@@ -75,13 +76,12 @@ const EditMovie = () => {
                 .then((data) => {
                     const checks = [];
 
-                    console.log("ini", data)
                     data.forEach(g => {
                         checks.push({ id: g.id, checked: false, genre: g.genre });
                     })
 
                     setMovie(m => ({
-                        ...movie,
+                        ...m,
                         genres: checks,
                         genres_array: [],
                     }))
@@ -97,6 +97,37 @@ const EditMovie = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        let errors = [];
+        let required = [
+            { field: movie.title, name: "title" },
+            { field: movie.release_date, name: "release_date" },
+            { field: movie.runtime, name: "runtime" },
+            { field: movie.description, name: "description" },
+            { field: movie.mpaa_rating, name: "mpaa_rating" },
+        ]
+
+        required.forEach(function (obj) {
+            if (obj.field === "") {
+                errors.push(obj.name);
+            }
+        })
+
+        if (movie.genres_array.length === 0) {
+            Swal.fire({
+                title: `Error!`,
+                text: `You must choose at least one genre!`,
+                icon: `error`,
+                confirmButtonText: `OK`,
+            })
+            errors.push("genres");
+        }
+
+        setErrors(errors);
+
+        if (errors.length > 0) {
+            return false;
+        }
     }
 
     const handleChange = () => (event) => {
@@ -113,12 +144,27 @@ const EditMovie = () => {
         console.log("value in handleCheck:", event.target.value);
         console.log("checked is", event.target.checked);
         console.log("position is", position);
+
+        let tmpArr = movie.genres;
+        tmpArr[position].checked = !tmpArr[position].checked
+
+        let tmpIDs = movie.genres_array;
+        if (!event.target.checked) {
+            tmpIDs.splice(tmpIDs.indexOf(event.target.value));
+        } else {
+            tmpIDs.push(parseInt(event.target.value, 10));
+        }
+
+        setMovie({
+            ...movie,
+            genres_array: tmpIDs,
+        })
     }
     return (
         <div>
             <h2>Add/Edit Movie</h2>
             <hr />
-            <pre>{JSON.stringify(movie, null, 3)}</pre>
+            {/* <pre>{JSON.stringify(movie, null, 3)}</pre> */}
 
             <form onSubmit={handleSubmit}>
 
@@ -196,6 +242,10 @@ const EditMovie = () => {
                         )}
                     </>
                 }
+
+                <hr />
+
+                <button className="btn btn-primary">Save</button>
 
             </form>
         </div>
