f9ea1d2059efb561e531dcabd15ac568256e23f6
sukma ilyas <sukmailyas9@gmail.com>
2023-08-15 20:07:36 +0800

    fixing bug

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -91,6 +91,47 @@ const EditMovie = () => {
                 })
         } else {
             // editing an existing movie
+
+            const headers = new Headers();
+            headers.append("Content-Type", "application/json");
+            headers.append("Authorization", "Bearer " + jwtToken);
+
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+            }
+
+            fetch(`/admin/movies/${id}`, requestOptions)
+                .then((response) => {
+                    if (response.status !== 200) {
+                        setErrors("Invalid response code: " + response.status)
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    // fix release date
+                    data.movie.release_date = new Date(data.movie.release_date).toISOString().split('T')[0];
+
+                    const checks = [];
+
+                    data.genres.forEach(g => {
+                        if (data.movie.genres_array.indexOf(g.id) !== -1) {
+                            checks.push({ id: g.id, checked: true, genre: g.genre });
+                        } else {
+                            checks.push({ id: g.id, checked: false, genre: g.genre });
+                        }
+                    })
+
+                    // set state
+                    setMovie({
+                        ...data.movie,
+                        genres: checks,
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+
         }
 
     }, [id, jwtToken, navigate])
@@ -154,7 +195,6 @@ const EditMovie = () => {
             headers: headers,
             credential: "include",
         }
-        console.log(requestOptions, "ija")
         fetch(`/admin/movies/${movie.id}`, requestOptions)
             .then((response) => response.json())
             .then((data) => {
